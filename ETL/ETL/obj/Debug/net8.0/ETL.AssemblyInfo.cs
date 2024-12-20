b954067deaa7a6c2c467aa87bfc5a7f8439942cc
Artem Matok <125476461+ArtemMatok@users.noreply.github.com>
2024-12-20 23:54:09 +0200

    Some changes

--- ETL/ETL/obj/Debug/net8.0/ETL.AssemblyInfo.cs
+++ ETL/ETL/obj/Debug/net8.0/ETL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ETL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60130124daf8713fcb1c5de3413a7b77e63c3736")]
 [assembly: System.Reflection.AssemblyProductAttribute("ETL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ETL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
